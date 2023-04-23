191e1be745b26add629c1df230a22ea40d1d12c6
ProgNeo <prograneo@gmail.com>
2023-04-24 01:42:15 +0800

    Optimize code

--- app/src/main/java/com/istu/schedule/util/LiveDataExt.kt
+++ app/src/main/java/com/istu/schedule/util/LiveDataExt.kt
@@ -2,35 +2,35 @@ package com.istu.schedule.util
 
 import androidx.lifecycle.MutableLiveData
 
-fun <T> MutableLiveData<MutableList<T>>.addNewItem(item: T) {
+inline fun <reified T> MutableLiveData<MutableList<T>>.addNewItem(item: T) {
     val newList = mutableListOf<T>()
     this.value?.let { newList.addAll(it) }
     newList.add(item)
     this.value = newList
 }
 
-fun <T> MutableLiveData<MutableList<T>>.addFrontItem(item: T) {
+inline fun <reified T> MutableLiveData<MutableList<T>>.addFrontItem(item: T) {
     val newList = mutableListOf<T>()
     newList.add(item)
     this.value?.let { newList.addAll(it) }
     this.value = newList
 }
 
-fun <T> MutableLiveData<MutableList<T>>.updateItemAt(item: T, index: Int) {
+inline fun <reified T> MutableLiveData<MutableList<T>>.updateItemAt(item: T, index: Int) {
     val newList = mutableListOf<T>()
     this.value?.let { newList.addAll(it) }
     newList[index] = item
     this.value = newList
 }
 
-fun <T> MutableLiveData<MutableList<T>>.insertItem(item: T, index: Int) {
+inline fun <reified T> MutableLiveData<MutableList<T>>.insertItem(item: T, index: Int) {
     val newList = mutableListOf<T>()
     this.value?.let { newList.addAll(it) }
     newList.add(index, item)
     this.value = newList
 }
 
-fun <T> MutableLiveData<MutableList<T>>.removeItem(item: T) {
+inline fun <reified T> MutableLiveData<MutableList<T>>.removeItem(item: T) {
     val newList = mutableListOf<T>()
     this.value?.let { newList.addAll(it) }
     newList.remove(item)

7f0e87c8672f970dac7c1cf17885fd9e80a672b0
ProgNeo <prograneo@gmail.com>
2023-07-09 13:55:46 +0800

    Fix horizontal calendar scrolling

--- app/src/main/java/com/istu/schedule/ui/components/calendar/HorizontalCalendar.kt
+++ app/src/main/java/com/istu/schedule/ui/components/calendar/HorizontalCalendar.kt
@@ -1,7 +1,7 @@
 package com.istu.schedule.ui.components.calendar
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,13 +10,15 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.istu.schedule.data.model.Week
 import java.time.LocalDate
+import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HorizontalCalendar(
     modifier: Modifier = Modifier,
@@ -26,15 +28,53 @@ fun HorizontalCalendar(
     calendarState: LazyListState,
     onSelect: (selectedDate: LocalDate) -> Unit
 ) {
-    val snapFlingBehavior = rememberSnapFlingBehavior(lazyListState = calendarState)
+    val lazyStackScope = rememberCoroutineScope()
+
+    val scrollSpeedDivisor = 1f
+    var sumScrollValue = 0f
 
     LazyRow(
         modifier = modifier
             .padding(vertical = 15.dp)
-            .fillMaxWidth(),
-//        contentPadding = PaddingValues(horizontal = 15.dp),
+            .fillMaxWidth()
+            .pointerInput(Unit) {
+                detectHorizontalDragGestures(
+                    onHorizontalDrag = { _, dragAmount ->
+                        lazyStackScope.launch {
+                            val scrollValue = -dragAmount / scrollSpeedDivisor
+                            calendarState.scrollBy(scrollValue)
+                            sumScrollValue += scrollValue
+                        }
+                    },
+                    onDragEnd = {
+                        lazyStackScope.launch {
+                            if (sumScrollValue > 200) {
+                                sumScrollValue = 0f
+                                calendarState.animateScrollToItem(
+                                    calendarState.firstVisibleItemIndex + 1
+                                )
+                            } else if (sumScrollValue < -200) {
+                                sumScrollValue = 0f
+                                calendarState.animateScrollToItem(
+                                    calendarState.firstVisibleItemIndex
+                                )
+                            } else if (sumScrollValue < 0) {
+                                sumScrollValue = 0f
+                                calendarState.animateScrollToItem(
+                                    calendarState.firstVisibleItemIndex + 1
+                                )
+                            } else {
+                                sumScrollValue = 0f
+                                calendarState.animateScrollToItem(
+                                    calendarState.firstVisibleItemIndex
+                                )
+                            }
+                        }
+                    }
+                )
+            },
         state = calendarState,
-        flingBehavior = snapFlingBehavior
+        userScrollEnabled = false
     ) {
         items(weeksList) {
             Box(modifier = Modifier.fillParentMaxWidth()) {
